1b1aee7fcd991e0e47b301c9e4d33af6a9609a1f
Liyo08 <liyotom04@gmail.com>
2024-07-19 20:52:44 +0530

    Routing completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
 import ViewMovie from './components/ViewMovie';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-    <AddMovie/> 
-    <ViewMovie/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+
+<Route path='/' element={<AddMovie/>}/>
+<Route path='/view' element={<ViewMovie/>}/>
+
+
+
+
+
+   </Routes>
+   </BrowserRouter>
   );
 }
 
--- src/components/MovieNavbar.jsx
+++ src/components/MovieNavbar.jsx
@@ -13,10 +13,10 @@ const MovieNavbar = () => {
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Add Movie</a>
+          <a class="nav-link active" aria-current="page" href="/">Add Movie</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">View All Movies</a>
+          <a class="nav-link" href="/view">View All Movies</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
