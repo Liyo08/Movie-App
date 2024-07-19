74d10de7c6108c564e090a596a8c814351c08d39
Liyo08 <liyotom04@gmail.com>
2024-07-19 20:38:48 +0530

    ViewMovie component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddMovie from './components/AddMovie';
+import ViewMovie from './components/ViewMovie';
 
 function App() {
   return (
     <div>
     <AddMovie/> 
+    <ViewMovie/>
     </div>
   );
 }
